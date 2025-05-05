59c77b9b5a13e23ed99e108d8c1fd8d10ca9d221
Pooja Nokhwal <poojanokhwal04@gmail.com>
2025-05-05 13:06:54 -0400

    Send Text as well as Image

--- MailingClientUsingPython/mailingclient.py
+++ MailingClientUsingPython/mailingclient.py
@@ -2,6 +2,7 @@ import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.mime.base import MIMEBase
+from email import encoders
 from dotenv import load_dotenv
 import os
 
@@ -11,15 +12,28 @@ email_address = os.getenv("EMAIL_ADDRESS")
 password = os.getenv("EMAIL_PASSWORD")
 toaddr = os.getenv("TOADDR")
 
+if not all([email_address, password, toaddr]):
+    print("Error: Email credentials are missing.")
+    exit()
+
 msg=MIMEMultipart()
-msg['FROM']='Pooja Nokhwal'
-msg['TO']='Pooja Nokhwal'
-msg['SUBJECT']='Testing the mail client'
+msg['FROM']='SENDER'
+msg['TO']='RECIEVER'
+msg['SUBJECT']='SUBJECT OF THE MAIL'
 
 with open('message.txt','r') as f:
     message=f.read()
 
+image='mention the path'
+with open(image, 'rb') as attach_image:
+    p = MIMEBase('application', 'octet-stream')
+    p.set_payload(attach_image.read())
+
+    encoders.encode_base64(p)
+    p.add_header('Content-Disposition', f'attachment; filename={image}')
+
 msg.attach(MIMEText(message, 'plain'))
+msg.attach(p)
 
 #you can write:- text=msg.as_string() and then in s.sendmail, replace msg.as_string() with just: text
 with smtplib.SMTP_SSL('smtp.gmail.com', 465) as s:
